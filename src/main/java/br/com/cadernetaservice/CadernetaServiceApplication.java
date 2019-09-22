857d98cd5db724f6b151e32f81890ddeca1b4131
Siron Azevedo <sirondba@gmail.com>
2019-09-22 18:05:12 -0300

    Ajustando o projeto

---
+++ src/main/java/br/com/cadernetaservice/CadernetaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadernetaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadernetaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadernetaServiceApplication.class, args);
+	}
+
+}
